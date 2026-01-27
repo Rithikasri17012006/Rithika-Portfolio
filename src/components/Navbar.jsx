aba8269e39774161be20ab7729e5e1a8ed9eac29
Rithikasri17012006 <rithikasrikarthikeyan5@gmail.com>
2026-01-27 14:31:29 +0530

    navbar hover changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const menu = [
   { label: "Home", id: "home" },
   { label: "Education", id: "education" },
   { label: "Certifications", id: "certifications" },
-  { label:"Skills",id:"skills"},
+  { label:"Skills",id:'skills'},
   { label: "Experience", id: "experience" },
   { label: "Projects", id: "projects" },
   { label: "Contact", id: "contact" }
@@ -15,23 +15,24 @@ export default function Navbar({ dark, setDark }) {
       {/* Menu */}
       <div className="flex gap-6">
         {menu.map((item) => (
-          <a
-            key={item.id}
-            href={`#${item.id}`}
-            className="hover:text-purple-400 transition"
-          >
+         <a
+  key={item.id}
+  href={`#${item.id}`}
+  className="
+    relative text-white transition-all duration-300
+    after:content-[''] after:absolute after:left-0 after:-bottom-1
+    after:w-0 after:h-[2px] after:bg-purple-400
+    after:transition-all after:duration-300
+    hover:after:w-full
+    hover:text-purple-400
+    hover:shadow-[0_0_20px_#a855f7]
+  "
+>
             {item.label}
           </a>
         ))}
       </div>
 
-      {/* Dark Mode Toggle */}
-      <button
-        onClick={() => setDark(!dark)}
-        className="px-4 py-1 rounded bg-purple-600"
-      >
-        {dark ? "Light Mode" : "Dark Mode"}
-      </button>
     </nav>
   );
 }
