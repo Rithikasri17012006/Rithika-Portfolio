acfb18034a98ba3ee7ff7c2e3d5e1cd64ac82285
Rithikasri17012006 <rithikasrikarthikeyan5@gmail.com>
2026-02-14 10:54:47 +0530

    certification is added

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -61,7 +61,12 @@ const certifications = [
     issuer: " Integra Connect",
     year: "2025",
   },
-  
+  ,
+    {
+    title: "Oracle Certified Professional – Java SE 17 Developer",
+    issuer: " Oracle",
+    year: "2026",
+  },
 ]
 
 
